7195366988b2f3fa24a7b89afd7e92c9df2c84ec
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-12-19 20:52:39 +0200

    props section complete

--- vanilla_svelte/src/routes/props/+page.svelte
+++ vanilla_svelte/src/routes/props/+page.svelte
@@ -1,6 +1,18 @@
 <script>
     import Nested from "./Nested.svelte";
+    import PackageInfo from "./PackageInfo.svelte";
+
+    const pkg = {
+        name: 'Svelte',
+        speed: 'Blazing',
+        version: 4,
+        website: 'https://svelte.dev'
+    };
 </script>
 
 <Nested answer={42} />
 <Nested />
+
+<PackageInfo 
+{...pkg}
+/>
