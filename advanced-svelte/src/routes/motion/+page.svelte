1bbd5420adbf320bc6ac12c877847d64a05902ec
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-12-21 19:40:59 +0200

    advanced svelte

---
+++ advanced-svelte/src/routes/motion/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { tweened } from "svelte/motion";
+    import { cubicOut } from "svelte/easing";
+
+    import Springs from "./Springs.svelte";
+
+    const progress = tweened(0 ,{
+        duration: 400,
+        easing: cubicOut
+    });
+</script>
+
+<progress value={$progress} />
+
+<button on:click={() => progress.set(0)}>
+	0%
+</button>
+
+<button on:click={() => progress.set(0.25)}>
+	25%
+</button>
+
+<button on:click={() => progress.set(0.5)}>
+	50%
+</button>
+
+<button on:click={() => progress.set(0.75)}>
+	75%
+</button>
+
+<button on:click={() => progress.set(1)}>
+	100%
+</button>
+
+<Springs/>
+
+<style>
+	progress {
+		display: block;
+		width: 100%;
+	}
+</style>
