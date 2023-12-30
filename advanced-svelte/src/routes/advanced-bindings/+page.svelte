ebbd4b6804828acfa4efca2e1c790789304e9d63
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-12-30 18:48:59 +0200

    dimensions complete

--- advanced-svelte/src/routes/advanced-bindings/+page.svelte
+++ advanced-svelte/src/routes/advanced-bindings/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import ContenteditableBindings from "./Contenteditable-Bindings.svelte";
     import EachBlockBindings from "./Each-Block-Bindings.svelte";
+    import Dimensions from "./Dimensions.svelte";
 </script>
 
 <ContenteditableBindings/>
 <EachBlockBindings/>
+<Dimensions/>
