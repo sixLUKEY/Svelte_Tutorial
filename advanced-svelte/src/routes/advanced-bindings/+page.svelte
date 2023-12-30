c9c355c4675463672fd30ae87a112a0cfab7cd15
Luke Evertson <130046695+sixLUKEY@users.noreply.github.com>
2023-12-30 13:33:20 +0200

    each block bindings

--- advanced-svelte/src/routes/advanced-bindings/+page.svelte
+++ advanced-svelte/src/routes/advanced-bindings/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import ContenteditableBindings from "./Contenteditable-Bindings.svelte";
+    import EachBlockBindings from "./Each-Block-Bindings.svelte";
 </script>
 
 <ContenteditableBindings/>
+<EachBlockBindings/>
